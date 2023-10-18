60b81b26b89792959ce07ef642adfd9ed86ceba2
Elian <eliangonniense@gmail.com>
2023-10-18 19:00:25 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,15 @@
+buttons = document.querySelectorAll(".slide-buttons li");
+maps = document.querySelectorAll(".slide li");
+
+const changeActive = (event) => {
+  index = event.currentTarget.id;
+  event.currentTarget.classList.remove("active");
+
+  maps.forEach((map) => map.classList.remove("active"));
+  maps[index - 1].classList.add("active");
+
+  buttons.forEach((button) => button.classList.remove("active"));
+  buttons[index - 1].classList.add("active");
+};
+
+buttons.forEach((button) => button.addEventListener("click", changeActive));

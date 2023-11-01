8475620661a12af3aab93efcc22c356d8e227b7b
Elian Silva <you@example.com>
2023-11-01 17:02:44 -0300

    faq adicionado

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
 buttons = document.querySelectorAll(".slide-buttons li");
 maps = document.querySelectorAll(".slide li");
 
+faqButtons = document.querySelectorAll(".faq li");
+
+const faqActive = (event) => {
+  index = event.currentTarget.id;
+  faqButtons[index - 1].classList.toggle("faq-active");
+};
+
+faqButtons.forEach((button) => button.addEventListener("click", faqActive));
+
 const changeActive = (event) => {
   index = event.currentTarget.id;
   event.currentTarget.classList.remove("active");
